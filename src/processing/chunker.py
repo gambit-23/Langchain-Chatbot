e4354de7930f22f2c9c2ab021ebefb6ea470762e
gambit-23 <sahilsharmapv@gmail.com>
2026-01-19 23:18:50 +0530

    Phase 2: src\processing\chunker.py ReadMe updated

--- src/processing/chunker.py
+++ src/processing/chunker.py
@@ -10,6 +10,9 @@ def chunk_text(texts: List[str], chunk_size: int=500, chunk_overlap: int=100) ->
     chunks:List[str]=[]
 
     for text in texts:
+        # texts -> list of full PDF texts, text -> one complete PDF
         split_chunks=splitter.split_text(text=text)
+        # split chunks takes a huge string, breaks into small chunks and returns a list
         chunks.extend(split_chunks)
+        # extend flattens all chunks into one list
     return chunks
